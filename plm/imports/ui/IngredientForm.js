d59f6625d8de821a9398359bb963bf853abe891a
Chad Coviel <cmc86@duke.edu>
2018-01-28 21:14:18 -0500

    Latest

--- plm/imports/ui/IngredientForm.js
+++ plm/imports/ui/IngredientForm.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import VendorForm from './VendorForm.js';
 import DropdownButton from '../../node_modules/react-bootstrap/lib/DropdownButton.js';
 import MenuItem from '../../node_modules/react-bootstrap/lib/MenuItem.js';
 import Button from '../../node_modules/react-bootstrap/lib/Button.js';
+// import { IngredientsList } from '../model/IngredientList.js';
  
 // onSubmit={this.handleSubmit.bind(this) -> Add later!
 
 // Task component - represents a single todo item
 export default class IngredientForm extends Component {
+	handleSubmit(event) {
+	    event.preventDefault();
+	 
+	    // Find the text field via the React ref
+	    const text = ReactDOM.findDOMNode(this.refs.ingredientName).value.trim();
+	    console.log(text);
+	    const storage = ReactDOM.findDOMNode(this.refs.packaging).value;
+	    console.log(storage);
+	    console.log(this.refs);
+	    // const storage = ReactDOM.findDOMNode(this.refs.temperatureState).value;
+	 
+	    // IngredientsList.insert({
+	    //   text,
+	    //   createdAt: new Date(), // current time
+	    // });
+	 
+	    // Clear form
+	    // ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  	}
   render() {
     return (
       <div>
-      <form className="new-task">
+      <form className="new-ingredient" onSubmit={this.handleSubmit.bind(this)}>
             <input
               type="text"
               ref="ingredientName"
@@ -22,6 +43,7 @@ export default class IngredientForm extends Component {
 				title={'primary'}
 				key={1}
 				id={`dropdown-basic-${1}`}
+				ref="packaging"
 			>
 				<MenuItem eventKey="1">Sack (50 lbs)</MenuItem>
 				<MenuItem eventKey="2">Pail (50 lbs)</MenuItem>
@@ -35,14 +57,13 @@ export default class IngredientForm extends Component {
 				title={'primary'}
 				key={2}
 				id={`dropdown-basic-${2}`}
+				ref="temperatureState"
 			>
 				<MenuItem eventKey="1">frozen</MenuItem>
 				<MenuItem eventKey="2">refrigerated</MenuItem>
 				<MenuItem eventKey="3">room temperature</MenuItem>
 			</DropdownButton>
-			<Button bsStyle="primary" bsSize="default">
-				Submit
-			</Button>
+			<input type="submit" value="Submit"/>
       </form>
       <VendorForm />
       </div>
