4acd93d6bd644e6e4163736353298b0318b55c23
Chad Coviel <cmc86@duke.edu>
2018-02-01 03:48:38 -0500

    Prepared database for bulk import, added pricing and numpackages fields, and addressed some edge cases.

---
+++ imports/ui/forms/Styles.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+export const selectStyle = {
+	display: 'block'
+};
+
+export const inputStyle = {
+	display: 'block'
+};
--- imports/utils/checks.js
+++ imports/utils/checks.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 // Short-circuiting, and saving a parse operation
 export function isInt(value) {
@@ -9,3 +10,15 @@ export function isInt(value) {
   x = parseFloat(value);
   return (x | 0) === x;
 }
+
+//Check if the ingredient exists in the database
+// export function isExistingIngredient(ingName) {
+// 	if (IngredientsList.find({name: ingName}).fetch() !== undefined && 
+// 		IngredientsList.findOne({name: ingName}).fetch().length != 0) {
+// 		console.log(IngredientsList.find({name: ingName}).fetch());
+// 		return true;
+// 	}
+// 	else {
+// 		return false;
+// 	}
+// }
