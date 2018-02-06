8e117a6d8f7efc53fe3802616b800ed73ef787fa
Chad Coviel <cmc86@duke.edu>
2018-02-06 03:34:49 -0500

    Fixed bug where item will remain in cart after it is removed rom database.

--- imports/api/Cart/Cart.js
+++ imports/api/Cart/Cart.js
@@ -1,17 +1,22 @@
 import { Mongo } from 'meteor/mongo';
 import { CartSchema, CartIngredientSchema } from '../Ingredients/Schemas.js';
+import IngredientsList from '../Ingredients/IngredientList.js';
 //Cart Database
+
+if (Meteor.isClient) {
+	Meteor.subscribe('ingredients');
+}
 
 Carts = new Mongo.Collection('carts');
 Carts.attachSchema(CartSchema);
 
 export default Carts;
 
-var cursor = Meteor.users.find();
+var cursor = IngredientsList.find();
 const handle = cursor.observeChanges({
-    'removed': function(id) {
-       // console.log('removing: '+id);
-       // IngredientsList.update({ }, {$pull : { "vendorInfo.vendor._id" : id}} , {multi : true})
+    'removed': function(id, ing) {
+       console.log("removing");
+       Carts.update({},{$pull : {ingredients : { "ingredient._id" : id}}});
     }
 });
 
