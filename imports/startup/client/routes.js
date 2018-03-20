5981c6af5616606809b95975236bcb152416f9da
Harry <cg173@duke.edu>
2018-03-19 23:39:47 -0400

    working

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,43 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
-import autoBind from 'react-autobind';
 import Authenticated from '../../ui/components/Authenticated/Authenticated.js'
 import Public from '../../ui/components/Public/Public.js'
 
 //pages
-
+import Index from '../../ui/pages/Index/Index.js'
 import Login from '../../ui/pages/Login/Login.js'
 import Logout from '../../ui/pages/Logout/Logout.js'
-import Index from '../../ui/pages/Index/Index.js'
-import CreateUser from '../../ui/pages/CreateUser/CreateUser.js'
-import AddVendor from '../../ui/pages/AddVendor/AddVendor.js'
-import EditVendor from '../../ui/pages/EditVendor/EditVendor.js';
-import EditCapacity from '../../ui/pages/EditCapacity/EditCapacity.js'
-import EditCapacities from '../../ui/pages/EditCapacities/EditCapacities.js'
-import VendorManagementHome from '../../ui/pages/VendorManagement/VendorManagementHome.js'
-import IngredientForm from '../../ui/forms/IngredientForm.js'
-import Table from '../../ui/table/Table.js';
-import BulkImportIngredients from '../../ui/pages/BulkImportIngredients/BulkImportIngredients.js';
-import AdminHomepage from '../../ui/pages/AdminHomepage/AdminHomepage.js';
-import UserHomepage from '../../ui/pages/UserHomepage/UserHomepage.js';
+
 import Homepage from '../../ui/pages/Homepage/Homepage.js'
+
 import InventoryManagementHome from '../../ui/pages/InventoryManagement/InventoryManagementHome.js';
-import AddIngredient from '../../ui/pages/AddIngredient/AddIngredient.js';
-import IngredientCart from '../../ui/pages/Cart/Cart.js';
-import SpendingReport from '../../ui/pages/SpendingReport/SpendingReport.js';
-import UserManagementHome from '../../ui/pages/UserManagement/UserManagementHome.js';
-import ManagerHomepage from '../../ui/pages/ManagerHomepage/ManagerHomepage';
+import EditCapacities from '../../ui/pages/EditCapacities/EditCapacities.js'
+import Cart from '../../ui/pages/Cart/Cart.js'
+
 import FormulaManagementHome from '../../ui/pages/FormulaManagement/FormulaManagementHome.js';
-import AddFormula from '../../ui/pages/AddFormula/AddFormula';
-import LogTable from '../../ui/syslog/LogTable.js';
-import ViewLogHome from '../../ui/pages/ViewLog/ViewLogHome.js';
 import EditFormula from '../../ui/pages/EditFormula/EditFormula.js'
 import ViewFormula from '../../ui/pages/ViewFormula/ViewFormula.js'
-import ProductionRun from '../../ui/pages/ProductionRun/ProductionRun.js'
-import ProductionReportPage from '../../ui/pages/ProductionReportPage/ProductionReportPage.js'
-import BulkImportFormulas from '../../ui/pages/BulkImportFormulas/BulkImportFormulas.js';
 
+import VendorManagementHome from '../../ui/pages/VendorManagement/VendorManagementHome.js'
+import EditVendor from '../../ui/pages/EditVendor/EditVendor.js';
+
+import UserManagementHome from '../../ui/pages/UserManagement/UserManagementHome.js';
+
+import ViewLogHome from '../../ui/pages/ViewLog/ViewLogHome.js';
 
 class App extends Component {
   constructor(props) {
@@ -49,34 +36,30 @@ class App extends Component {
     const { props, state, setAfterLoginPath } = this;
     return (
   
-      <Router>
+    <Router>
       <div>
-      <Switch>
-        <Route exact name="index" path="/" component={Index} />
-        <Route path="/login" component={Login} />
-        <Route path="/logout" component={Logout} />
+        <Switch>
+          <Route exact name="index" path="/" component={Index} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
 
-        <Authenticated exact path="/homepage" component={Homepage} {...props}/>
+          <Authenticated exact path="/homepage" component={Homepage} {...props}/>
 
-        <Authenticated exact path='/inventoryManagement' component={InventoryManagementHome} {...props}/>
-        <Authenticated exact path="/editCapacities" component={EditCapacities} {...props}/>
-        <Authenticated exact path="/cart" component={IngredientCart} {...props}/>
+          <Authenticated exact path='/inventoryManagement' component={InventoryManagementHome} {...props}/>
+          <Authenticated exact path="/editCapacities" component={EditCapacities} {...props}/>
+          <Authenticated exact path="/cart" component={Cart} {...props}/>
 
-        <Authenticated exact path='/formulaManagement' component={FormulaManagementHome} {...props}/>
-        <Authenticated exact path='/editFormula/:formulaID' component={EditFormula} {...props}/>
-        <Authenticated exact path='/viewFormula/:formulaID' component={ViewFormula} {...props}/> 
+          <Authenticated exact path='/formulaManagement' component={FormulaManagementHome} {...props}/>
+          <Authenticated exact path='/editFormula/:formulaID' component={EditFormula} {...props}/>
+          <Authenticated exact path='/viewFormula/:formulaID' component={ViewFormula} {...props}/> 
 
-        <Authenticated exact path="/vendorManagement" component={VendorManagementHome} {...props}/>
-        <Authenticated exact path="/editVendor/:vendorID" component={EditVendor} {...props}/>
+          <Authenticated exact path="/vendorManagement" component={VendorManagementHome} {...props}/>
+          <Authenticated exact path="/editVendor/:vendorID" component={EditVendor} {...props}/>
 
-        <Authenticated exact path="/userManagement" component={UserManagementHome} {...props}/>
+          <Authenticated exact path="/userManagement" component={UserManagementHome} {...props}/>
 
-
-        <Authenticated exact path='/viewLog' component={ViewLogHome} {...props}/>
-
-
+          <Authenticated exact path='/viewLog' component={ViewLogHome} {...props}/>
         </Switch>
-           
       </div>
       
     </Router>
