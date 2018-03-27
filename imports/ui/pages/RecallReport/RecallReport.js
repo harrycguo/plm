81f1120f01f4ec84df893da461f08f83d73737e6
dwe8 <dwe8@duke.edu>
2018-03-27 05:58:31 -0400

    recall report

--- imports/ui/pages/RecallReport/RecallReport.js
+++ imports/ui/pages/RecallReport/RecallReport.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
+import { button } from 'react';
+import ReactTable from 'react-table';
+import { Button , ButtonToolbar, ControlLabel, Glyphicon } from 'react-bootstrap';
 
-class RecallReport extends Component {
-  constructor(props) {
-    super(props);
-    
+import IngredientList from '../../../api/Ingredients/IngredientList.js';
+import { Intermediates } from '../../../api/Intermediates/intermediates.js'
+import RecallReport from '../../../api/RecallReport/RecallReport.js'
+import Formulas from '../../../api/Formulas/formulas.js'
+
+class RecallReportView extends Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		console.log("Recall Report's This:")
+		console.log(this)
+    	return (
+      	<div>
+      	<table>
+      	<tbody>
+      		<tr>
+      			<th>Name</th>
+      			<th>asdf</th>
+      		</tr>
+      	</tbody>
+      	</table>
+
+        	Recall Report Goes Here
+      	</div>
+    	);
+  	}
 }
 
-  render() {
-    return (
-      <div>
-          Recall Report Goes Here
-      </div>
-    );
-  }
-}
+export default withTracker(() => {
+	Meteor.subscribe('recallreport')
+	Meteor.subscribe('ingredients')
+	Meteor.subscribe('intermediates')
+	Meteor.subscribe('formulas')
+	return {
+		ingredients: IngredientList.find({}).fetch(),
+		intermediates: Intermediates.find({}).fetch(),
+		formulas: Formulas.find({}).fetch(),
+		recallreport: RecallReport.find({}).fetch(),
 
-export default RecallReport;
-
+		// intermediates: Intermediates.find({}).fetch()
+	};
+})(RecallReportView);
 
 
 
